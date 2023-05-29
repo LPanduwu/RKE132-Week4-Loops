849550961f9d62a9dcd2a42800e59e34d1107b37
LPanduwu <liisbeth.prangli@tktk.ee>
2023-05-29 15:53:34 +0300

    add week4loops code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,8 @@
+﻿int sum = 0;
+for (int i = 1; i < 11; i++) //i-- = i - 1
+{
+    Console.WriteLine($"i = {i}");
+    sum += i; //sum = sum + i;
+    Console.WriteLine($"Current total: {sum}");
+}
+Console.WriteLine($"Final total: {sum}");
---
+++ RandomNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿Random rnd= new Random();
+int myRandomNum;
+int randomSum = 0;
+for(int i = 0; i < 3; i++)
+{
+    myRandomNum= rnd.Next(0, 11); // 0 - 10
+    randomSum+= myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+Console.WriteLine($"Random number total: {randomSum}");
